53458d15779dfa5832abf38189614b770d8b1cde
Felipe <felipejm9805@gmail.com>
2020-08-02 15:37:50 -0700

    Props

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -5,6 +5,8 @@ import confLogo from "../images/badge-header.svg";
 
 class Badge extends React.Component {
   render() {
+    const { firstName, lastName, avatarUrl, jobTitle, twitter } = this.props;
+
     return (
       <div className="Badge">
         <div className="Badge__header">
@@ -12,21 +14,17 @@ class Badge extends React.Component {
         </div>
 
         <div className="Badge__section-name">
-          <img
-            className="Badge__avatar"
-            src="https://s.gravatar.com/avatar/eef6e63767be8cd49add27c035589294?s=80"
-            alt="Avatar"
-          ></img>
+          <img className="Badge__avatar" src={avatarUrl} alt="Avatar"></img>
           <h1>
-            Felipe
+            {firstName}
             <br />
-            Jurado
+            {lastName}
           </h1>
         </div>
 
         <div className="Badge__section-info">
-          <h3>Ingeniero</h3>
-          <div>@felipej9805</div>
+          <h3>{jobTitle}</h3>
+          <div>@{twitter}</div>
         </div>
 
         <div className="Badge__footer">#Platzi</div>
--- src/index.js
+++ src/index.js
@@ -17,4 +17,9 @@ import Badge from "./components/Badge";
 const container = document.getElementById("app");
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(<Badge />, container);
+ReactDOM.render(<Badge
+    firstName="Felipe"
+    lastName="Jurado"
+    avatarUrl="https://s.gravatar.com/avatar/eef6e63767be8cd49add27c035589294?s=80"
+    jobTitle="Site Reliability Engineer"
+    twitter="felipej9805" />, container);
