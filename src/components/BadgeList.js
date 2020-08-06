7ef4ec65a87f23d8499be1e22cdab30006a8100e
Felipe <felipejm9805@gmail.com>
2020-08-05 18:06:13 -0700

    Intentando traer los datos de Rick & Morty API - ERROR

--- src/components/BadgeList.js
+++ src/components/BadgeList.js
@@ -1,24 +1,30 @@
 import React from 'react';
 
 import '../styles/BadgesList.css';
+//import Loader from '../components/Loader'
 
 class BadgesListItem extends React.Component {
+
+
+
     render() {
+        console.log("-------------------------");
+        console.log(this.props.badge.results);
         return (
             <div className="BadgesListItem">
                 <img
                     className="BadgesListItem__avatar"
-                    src={this.props.badge.avatarUrl}
-                    alt={`${this.props.badge.firstName} ${this.props.badge.lastName}`}
+                    src={this.props.badge.results[3].image}
+                    alt={`${this.props.badge.name} ${this.props.badge.name}`}
                 />
 
                 <div>
                     <strong>
-                        {this.props.badge.firstName} {this.props.badge.lastName}
+                        {this.props.badge.name} {this.props.badge.name}
                     </strong>
-                    <br />@{this.props.badge.twitter}
+                    <br />@{this.props.badge.status}
                     <br />
-                    {this.props.badge.jobTitle}
+                    {this.props.badge.species}
                 </div>
             </div>
         );
@@ -27,18 +33,35 @@ class BadgesListItem extends React.Component {
 
 class BadgesList extends React.Component {
     render() {
+        // if (this.state.error) {
+        //     return `Error: ${this.state.error.message}`
+        // }
+
         return (
             <div className="BadgesList">
                 <ul className="list-unstyled">
                     {this.props.badges.map(badge => {
+                        console.log(badge)
+
                         return (
-                            <li key={badge.id}>
+
+                            <li key={badge.id} >
                                 <BadgesListItem badge={badge} />
                             </li>
                         );
                     })}
                 </ul>
-            </div>
+
+                {/* {this.state.loading && (
+                    <div className="loader">
+                        <Loader />
+                    </div>
+                )} */}
+
+                {/* {!this.state.loading && (
+    <button onClick={this.fet}></button>
+)} */}
+            </div >
         );
     }
 }
--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -9,39 +9,109 @@ import './styles/Badges.css'
 import { Link } from 'react-router-dom'
 
 class Badges extends Component {
-    state = {
-        data: [
-            {
-                id: "2de30c42-9deb-40fc-a41f-05e62b5939a7",
-                firstName: "Freda",
-                lastName: "Grady",
-                email: "Leann_Berge@gmail.com",
-                jobTitle: "Legacy Brand Director",
-                twitter: "FredaGrady22221-7573",
-                avatarUrl: "https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon"
+
+
+    constructor(props) {
+        super(props);
+        console.log("1. constructor()");
+
+        this.state = {
+            nextPage: 1,
+            loading: true,
+            error: null,
+            data: {
+                info: {},
+                results: {},
             },
-            {
-                id: "d00d3614-101a-44ca-b6c2-0be075aeed3d",
-                firstName: "Major",
-                lastName: "Rodriguez",
-                email: "Ilene66@hotmail.com",
-                jobTitle: "Human Research Architect",
-                twitter: "ajorRodriguez61545",
-                avatarUrl: "https://www.gravatar.com/avatar/d57a8be8cb9219609905da25d5f3e50a?d=identicon"
-            },
-            {
-                id: "63c03386-33a2-4512-9ac1-354ad7bec5e9",
-                firstName: "Daphney",
-                lastName: "Torphy",
-                email: "Ron61@hotmail.com",
-                jobTitle: "National Markets Officer",
-                twitter: "DaphneyTorphy96105",
-                avatarUrl: "https://www.gravatar.com/avatar/e74e87d40e55b9ff9791c78892e55cb7?d=identicon"
-            }
-        ]
+        }
     }
 
+    componentDidMount() {
+        console.log("3. componentDidMount()");
+        this.fetchCharacters()
+    }
+
+    fetchCharacters = async () => {
+        this.setState({ loading: true, error: null })
+
+        try {
+            const response = await fetch('https://rickandmortyapi.com/api/character/');
+            const data = await response.json();
+            console.log(data);
+            this.setState({
+                loading: false,
+                data: {
+                    info: data.info,
+                    results: [].concat(this.state.data.results, data.results),
+                },
+                nextPage: this.state.nextPage + 1,
+            });
+        } catch (error) {
+            this.setState({
+                loading: false,
+                error: error,
+            });
+        }
+
+
+    }
+
+    // componentDidMount() {
+    //     console.log("3. componentDidMount()");
+    //     this.timeoutId = setTimeout(() => {
+    //         this.setState({
+    //             data: [
+    //                 {
+    //                     id: "2de30c42-9deb-40fc-a41f-05e62b5939a7",
+    //                     firstName: "Freda",
+    //                     lastName: "Grady",
+    //                     email: "Leann_Berge@gmail.com",
+    //                     jobTitle: "Legacy Brand Director",
+    //                     twitter: "FredaGrady22221-7573",
+    //                     avatarUrl: "https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon"
+    //                 },
+    //                 {
+    //                     id: "d00d3614-101a-44ca-b6c2-0be075aeed3d",
+    //                     firstName: "Major",
+    //                     lastName: "Rodriguez",
+    //                     email: "Ilene66@hotmail.com",
+    //                     jobTitle: "Human Research Architect",
+    //                     twitter: "ajorRodriguez61545",
+    //                     avatarUrl: "https://www.gravatar.com/avatar/d57a8be8cb9219609905da25d5f3e50a?d=identicon"
+    //                 },
+    //                 {
+    //                     id: "63c03386-33a2-4512-9ac1-354ad7bec5e9",
+    //                     firstName: "Daphney",
+    //                     lastName: "Torphy",
+    //                     email: "Ron61@hotmail.com",
+    //                     jobTitle: "National Markets Officer",
+    //                     twitter: "DaphneyTorphy96105",
+    //                     avatarUrl: "https://www.gravatar.com/avatar/e74e87d40e55b9ff9791c78892e55cb7?d=identicon"
+    //                 },
+    //             ],
+    //         });
+    //     }, 3000);
+    // }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log("5. componentDidUpdate()");
+        console.log({
+            prevProps: prevProps, prevState: prevState
+        });
+        console.log({
+            props: this.props,
+            state: this.state,
+        });
+    }
+
+    componentWillUnmount() {
+        console.log("6. componentWillUnmount");
+        clearTimeout(this.timeoutId);
+
+    }
     render() {
+        console.log("2/4. render()");
+
         return (
             <React.Fragment>
 
@@ -63,8 +133,6 @@ class Badges extends Component {
                     <div className="Badges__list">
                         <div className="Badges__container">
                             <BadgesList badges={this.state.data} />
-
-
 
                         </div>
                     </div>
